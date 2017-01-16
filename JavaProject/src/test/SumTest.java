ae0e9685795e48c51b9167fde27b4c51f6a17a93
it <it@DESKTOP-KKAQ0RK>
2017-01-16 11:48:41 +0900

    commit

---
+++ JavaProject/src/test/SumTest.java
@@ -0,0 +1,14 @@
+package test;
+
+public class SumTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		
+		System.out.println(a+b);
+
+	}
+
+}
