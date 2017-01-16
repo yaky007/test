c9c8fe23de9c76cc9d59137f65a79fdec99bcdfc
it <it@DESKTOP-KKAQ0RK>
2017-01-16 14:09:00 +0900

    commit

---
+++ ArrayTest/src/test/SortTest.java
@@ -0,0 +1,15 @@
+package test;
+
+public class SortTest {
+
+	public static void main(String[] args) {
+		int [] nums={57,5,10,7,63,9};
+		sort(nums,1);
+		sort(nums,2);
+
+	}
+	private static void sort(int [] nums, int orderby){
+		
+	}
+
+}
