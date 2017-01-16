e69b9480b87b30dd6911f0601f798de56d4f1a9f
it <it@DESKTOP-KKAQ0RK>
2017-01-16 14:19:19 +0900

    commit

--- ArrayTest/src/test/SortTest.java
+++ ArrayTest/src/test/SortTest.java
@@ -8,8 +8,35 @@ public class SortTest {
 		sort(nums,2);
 
 	}
-	private static void sort(int [] nums, int orderby){
-		
+private static void sort(int [] nums, int orderby){
+		//다운받은곳에서 수정
+		if(orderby==1){
+			for(int i=0;i<nums.length-1;i++){
+				for(int j=i+1;j<nums.length;j++){
+					if(nums[i]>nums[j]){
+						int tmp=nums[i];//tmp=57;
+						nums[i]=nums[j];//nums[0]=5;
+						nums[j]=tmp;//nums[1]=57;
+					}
+				}// for j
+			}//for i
+			for(int i=0;i<nums.length;i++){
+				System.out.println("오름차순 : " + nums[i]+" ");
+			}
+		}//if
+		else if(orderby==2){
+			for(int i=0;i<nums.length-1;i++){
+				for(int j=i+1;j<nums.length;j++){
+					if(nums[i]<nums[j]){
+						int tmp=nums[i];//tmp=57;
+						nums[i]=nums[j];//nums[0]=5;
+						nums[j]=tmp;//nums[1]=57;
+					}
+				}// for j
+			}//for i
+			for(int i=0;i<nums.length;i++){
+				System.out.println("내림차순 : " + nums[i]+" ");
+			}
+		}
 	}
-
 }
